9b24ef1ba2428860453fcef7f1bf9b60114c9c39
Jacob Moulton <Jacob.Moulton@gmail.com>
2018-11-05 09:27:20 -0700

    started on board

--- board.py
+++ board.py
@@ -1,2 +1,12 @@
+from piece import Pawn, Color
+
 class Board:
-    pass
+    pieces = []
+    def __init__(self):
+        #make pawns
+        for i in range(8):
+            self.pieces.append(Pawn((i,1),Color.WHITE,self))
+            self.pieces.append(Pawn((i,6),Color.BLACK,self))
+        #make white pieces
+        
+        #make black pieces
